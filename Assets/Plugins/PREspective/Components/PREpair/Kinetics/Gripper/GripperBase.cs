5d03d6821f634e9ec8bd14019536c5507277a127
Oscar <oacalee@gmail.com>
2020-04-27 17:34:55 -0700

    New repair shop

---
+++ Assets/Plugins/PREspective/Components/PREpair/Kinetics/Gripper/GripperBase.cs
@@ -0,0 +1,131 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using u040.prespective.prepair.inspector;
+using u040.prespective.utility.editorscripts.customAttributes;
+using UnityEngine;
+
+namespace u040.prespective.prepair.physics.kinetics.gripper
+{
+
+    public class GripperBase : BaseGripperBase, IControlPanel
+    {
+        [SerializeField] private int toolbarTab;
+
+        //Required components
+        private GripperFinger mainFinger;
+        private GripperFinger secondaryFinger;
+
+        //Settings
+        [ButtonAttribute("toggleGripperClose")] public bool ToggleGripper = false;
+        [Range(0f, 1f)] public float CloseFactor = 0f;
+        public float ClosingSpeed = 0.5f; //own length/sec
+        public bool DEBUG = true;
+
+
+        //Data
+        [ReadOnly] public bool iClosed;
+
+        protected override void initializeGripper()
+        {
+            base.initializeGripper();
+
+            while (this.Fingers.Count < 2)
+            {
+                this.Fingers.Add(null);
+            }
+            this.mainFinger = (GripperFinger)this.Fingers[0];
+            this.secondaryFinger = (GripperFinger)this.Fingers[1];
+        }
+
+        protected override void moveFingersInPosition(float _deltaTime)
+        {
+            if (!isInPosition())
+            {
+                float distanceFromMovePoint = (CloseFactor - mainFinger.prismaticJoint.CurrentPerc) * mainFinger.prismaticJoint.ConstrainingSpline.SplineLength; //Distance between current position and closeFactorpoint to move to
+                int direction = Math.Sign(distanceFromMovePoint); //direction to move in
+                float intendedMove = ClosingSpeed * mainFinger.prismaticJoint.ConstrainingSpline.SplineLength * _deltaTime * direction; //Calculated the inteded move step
+
+                mainFinger.prismaticJoint.KinematicTranslation(Math.Abs(distanceFromMovePoint) > Math.Abs(intendedMove) ? intendedMove : distanceFromMovePoint, utility.VectorSpace.LocalParent, new Action<float, Vector3>((float percentage, Vector3 translation) => { /*Callback here*/ })); //apply step
+            }
+        }
+
+        protected override void processGrip()
+        {
+            //If detected both same objects, not null, and not yet registered
+            if ((mainFinger.DetectedObject == secondaryFinger.DetectedObject) && (mainFinger.DetectedObject != GrippedObject) && (mainFinger.DetectedObject != null))
+            {
+                CloseFactor = mainFinger.prismaticJoint.CurrentPerc;
+                List<Vector3[]> _cols = new List<Vector3[]>() { mainFinger.TriggerCollisionPoints, secondaryFinger.TriggerCollisionPoints };
+
+                GrippedObject = mainFinger.DetectedObject;
+                RegisteredLocalPosition = transform.InverseTransformPoint(GrippedObject.transform.position); //Convert grippedObject worldspace to localspace point
+                RegisteredRotation = Quaternion.Inverse(transform.rotation) * GrippedObject.transform.rotation;
+
+                DBLog.Regular(this, "Gripped object: " + GrippedObject.name + ", registeredPosition: " + RegisteredLocalPosition.ToString(), DEBUG);
+
+            }
+
+            //If both claw do not detect the grippedObject anymore
+            else if (mainFinger.DetectedObject != GrippedObject && secondaryFinger.DetectedObject != GrippedObject && GrippedObject != null)
+            {
+                DBLog.Regular(this, "Release", DEBUG);
+                GrippedObject = null;
+                RegisteredLocalPosition = transform.localPosition; //reset to own position
+                RegisteredRotation = transform.localRotation;
+            }
+
+            //If claws touch
+            else if ((mainFinger.DetectedObject == secondaryFinger.gameObject || secondaryFinger.DetectedObject == mainFinger.gameObject) && CloseFactor != 0)
+            {
+                DBLog.Warning(this, "Claws made contact. Opening gripper.");
+                closeGripper(false);
+            }
+
+            setClosed(GrippedObject != null || (isInPosition() && CloseFactor == 1f));
+        }
+
+        bool isInPosition()
+        {
+            return CloseFactor >= (mainFinger.prismaticJoint.CurrentPerc - 0.001f) && CloseFactor <= (mainFinger.prismaticJoint.CurrentPerc + 0.001f); //Return true when distance between points is 0
+        }
+
+
+        public void toggleGripperClose()
+        {
+            closeGripper(!ToggleGripper);
+        }
+
+        public void closeGripper(bool state)
+        {
+            if (state != ToggleGripper)
+            {
+                ToggleGripper = state;
+                CloseFactor = ToggleGripper ? 1 : 0;
+                DBLog.Regular(this, ToggleGripper ? "Closing gripper" : "Opening gripper", DEBUG);
+            }
+        }
+        
+        void setClosed(bool _state)
+        {
+            if (iClosed != _state) iClosed = _state;
+        }
+
+
+        public void ShowControlPanel()
+        {
+#if UNITY_EDITOR || UNITY_EDITOR_BETA
+            CloseFactor = EditorGUILayout.Slider("Close factor", CloseFactor, 0f, 1f);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Toggle Gripper"))
+            {
+                toggleGripperClose();
+            }
+            GUILayout.EndHorizontal();
+#endif
+        }
+    }
+}
---
+++ Assets/Plugins/PREspective/Components/PREpair/Kinetics/Gripper/GripperFinger.cs
@@ -0,0 +1,90 @@
+﻿using u040.prespective.prepair.kinematics;
+using UnityEngine;
+
+namespace u040.prespective.prepair.physics.kinetics.gripper
+{
+    public class GripperFinger : BaseGripperFinger
+    {
+        //Required components
+        public PrismaticJoint prismaticJoint;
+
+        //Settings
+        public bool DEBUG = false;
+
+        protected override void autoDetectColliderAndTrigger() //Automatically detect all required components
+        {
+            bool _detectedAllComponents = true;
+            #region<<PrismaticJoint>>
+
+            prismaticJoint = this.RequireComponent<PrismaticJoint>(true);
+            if (prismaticJoint == null) { _detectedAllComponents = false; }
+
+            #endregion
+            base.autoDetectColliderAndTrigger();
+            #region<<Collider>>
+            Collider[] col = GetComponentsInChildren<Collider>();
+            if (col.Length > 2)
+            {
+                string jointNames = "";
+                for (int i = 0; i < col.Length; i++)
+                {
+                    jointNames += col[i].name + (i < col.Length - 1 ? ", " : ".");
+                }
+                DBLog.Warning(this, "Cannot automatically detect 'Collider'. Multiple 'Collider' detected on: " + jointNames);
+                _detectedAllComponents = false;
+            }
+            else if (col.Length == 2)
+            {
+                if (col[0].isTrigger != col[1].isTrigger)
+                {
+                    TriggerBox = col[0].isTrigger ? col[0] : col[1];
+                    ColliderBox = col[0].isTrigger ? col[1] : col[0];
+                }
+                else
+                {
+                    string jointNames = "";
+                    for (int i = 0; i < col.Length; i++)
+                    {
+                        jointNames += col[i].name + (i < col.Length - 1 ? ", " : ".");
+                    }
+                    DBLog.Warning(this, "Cannot automatically detect 'Collider'. Multiple 'Collider' detected on: " + jointNames);
+                    _detectedAllComponents = false;
+                }
+            }
+            else if (col.Length == 1)
+            {
+                if (col[0].isTrigger)
+                {
+                    TriggerBox = col[0];
+                    DBLog.Warning(this, "Cannot automatically detect 'Collider'. No non-trigger collider detected.");
+                    _detectedAllComponents = false;
+                }
+                else
+                {
+                    ColliderBox = col[0];
+                    DBLog.Warning(this, "Cannot automatically detect 'Trigger'. No trigger collider detected.");
+                    _detectedAllComponents = false;
+                }
+            }
+            else
+            {
+                DBLog.Warning(this, "Cannot automatically detect 'Collider'. No 'Collider' detected.");
+                _detectedAllComponents = false;
+            }
+            #endregion
+            if (_detectedAllComponents) DBLog.Regular(this, "Automatically detected all components.", DEBUG);
+        }
+
+        protected override void onTriggerEnterInput(Collider _other)
+        {
+            base.onTriggerEnterInput(_other);
+            DBLog.Regular(this, "Collider: " + DetectedObject.name, DEBUG);
+        }
+
+        protected override void onTriggerExitInput(Collider _other)
+        {
+            base.onTriggerExitInput(_other);
+            if (DetectedObject != null) DBLog.Regular(this, "Released " + DetectedObject.name, DEBUG);
+        }
+    }
+}
