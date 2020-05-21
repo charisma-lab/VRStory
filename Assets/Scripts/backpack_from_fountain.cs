bb98096a96b5b9d885740ac6a5651c6d2755b48b
Neranjan <neranjan.kuppuswamy@gmail.com>
2020-05-21 15:33:21 -0700

    Added small animation, so robot can present the backpack

---
+++ Assets/Scripts/backpack_from_fountain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backpack_from_fountain : MonoBehaviour
+{
+    public Animator anim;   // ref to female villain's animator
+    public bool villainMoving = false;  // to prevent double taps
+    public GameObject theNote;
+    public GameObject theBackpack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            Debug.Log("roboCop Final");
+            anim.Play("backpack_present");
+        }
+    }
+
+    /*
+    // Function called when villain has moved out of the room
+    void villainLeft()
+    {
+        villainMoving = false;
+    }
+
+    // Activate Note and 
+    void showNote()
+    {
+        theNote.SetActive(true);
+    }
+
+    //Deactivate Backpack
+    void hideBackpack()
+    {
+        theBackpack.SetActive(false);
+    }
+    */
+}
