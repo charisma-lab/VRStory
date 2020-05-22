3ab6ea90c27247bfd2e165b819c8fc34cb282bce
Chinmay Wadgaonkar <wadgaonc@oregonstate.edu>
2020-05-21 18:03:02 -0700

    Basic robot animation stuff

---
+++ Assets/Scripts/robot_classroom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot_classroom : MonoBehaviour
+{
+    public Animator anim;   // ref to robot's animator
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            Debug.Log("Robot triggered!");
+            anim.Play("robot_classroom_enter");
+        }
+    }
+}
