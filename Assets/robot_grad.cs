836c864b1f683ca745fdb782afb145a7ebf1071e
Oscar <oacalee@gmail.com>
2020-07-20 17:24:09 -0700

    add stuff

---
+++ Assets/robot_grad.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot_grad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+   
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Debug.Log("RobotComeIn");
+            anim.SetBool("RobotComeIn", true);
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            Debug.Log("robotMove1");
+            anim.SetBool("robotMove1", true);
+        }
+
+        if (Input.GetKeyDown("5"))
+        {
+            Debug.Log("robotMove2");
+            anim.SetBool("robotMove2", true);
+        }
+        if (Input.GetKeyDown("6"))
+        {
+            Debug.Log("robotRunAway");
+            anim.SetBool("robotRunAway", true);
+        }
+    }
+}
---
+++ Assets/vilian_grad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vilian_grad : MonoBehaviour
+{
+    public Animator anim;
+    public AudioSource shopkeeper_voice;
+    public AudioClip Voice;
+    //public AudioClip shopkeeper_Clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        if (Input.GetKeyDown("2"))
+        {
+            Debug.Log("UserCome");
+            anim.SetBool("UserCome", true);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            Debug.Log("VillianSpeak");
+            anim.SetBool("VillianSpeak", true);
+        }
+    }
+}
